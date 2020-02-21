f2e0c43d785a2e07aabe5d03fcb5d6cd283b9807
Gerald McAuley <geraldmcauley@gwsteam.com>
2020-02-21 18:23:53 +0000

    adding routes

--- app.py
+++ app.py
@@ -13,6 +13,19 @@ def index():
     boards = query_db('select * from boards')
     return render_template('homepage.html',boards=boards)
 
+@app.route('/<board>')
+def board(board):
+    posts = query_db('select * from posts where board = "{}"'.format(board))
+    return render_template('board.html',posts=posts,board=board)
+
+@app.route('/<board>/post', methods=['POST'])
+def post(board):
+    filename = ''
+    now = datetime.datetime.now()
+    post = (request.form.get('name'),now.isoformat(),board,request.form.get('post_text'),filename)
+    # print create_post(post)
+    return 'yay'
+
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
@@ -26,6 +39,13 @@ def query_db(query, args=(), one=False):
     cur.close()
     return (rv[0] if rv else None) if one else rv
 
+def create_post(request):
+    query = '''INSERT INTO posts(user, data, board, post_text,image_file)values(?,?,?,?,?)'''
+    cur = get_db().cursor()
+    cur.execute(query,request)
+    get_db().commit()
+    cur.close()
+    return cur.lastrowid
 
 if __name__ == '__main__':
     app.run()
